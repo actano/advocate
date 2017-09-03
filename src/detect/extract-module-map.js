85d970820b37431d8aaa7edb2e8b602c82df2da5
Marcus Mennemeier <m.mennemeier@global-communication.de>
2017-09-03 21:35:34 +0200

    Be robust against [Circular] output from npm

--- src/detect/extract-module-map.js
+++ src/detect/extract-module-map.js
@@ -1,60 +1,81 @@
 import isObject from 'lodash/isObject'
-import mergeWith from 'lodash/mergeWith'
+import isEqual from 'lodash/isEqual'
 import uniq from 'lodash/uniq'
-import values from 'lodash/values'
 
 import guessModuleLicense from './guess-module-license'
 
-const _getExplicitModuleName = module => `${module.name}@${module.version}`
-
-const _mergeArray = (a, b) => (Array.isArray(a) ? uniq(a.concat(b)) : undefined)
-
 const _extractLicenseName = (license) => {
+  if (!license) return null
   if (Array.isArray(license)) return license.map(_extractLicenseName)
-  if (isObject(license)) return license.type || license.name
+  if (isObject(license)) return _extractLicenseName(license.type || license.name)
   return license
 }
 
-const _mapModule = (module, parents) => {
-  const explicitLicense = _extractLicenseName(
-    module.license || module.licence || module.licenses || module.licences)
-  const guessedLicense = explicitLicense ? null : guessModuleLicense(module)
-  const isLicenseGuessed = !!guessedLicense
-  const licenseDescriptor = explicitLicense || guessedLicense
-
-  return {
-    name: module.name,
-    explicitName: _getExplicitModuleName(module),
-    version: module.version,
-    licenseDescriptor,
-    isLicenseGuessed,
-    installPaths: [module.path],
-    dependencyPaths: [parents],
-  }
+const check = (a, b, name) => {
+  if (!isEqual(a, b)) throw new Error(`${name} mismatch: ${typeof a} ${a} !== ${typeof b} ${b}`)
 }
 
-function* iterateModules(module, parentPath) {
-  const main = _mapModule(module, parentPath)
-  const { explicitName } = main
+const merge = (a, b) => {
+  if (!b) return a
+  if (!a) return b
+  for (const prop of ['name', 'version', 'explicitName', 'licenseDescriptor', 'isLicenseGuessed']) {
+    check(a[prop], b[prop], prop)
+  }
+  const installPaths = uniq(a.installPaths.concat(b.installPaths))
+  const dependencyPaths = uniq(a.dependencyPaths.concat(b.dependencyPaths))
+  return { ...a, installPaths, dependencyPaths }
+}
+
+function* iterateModules(name, module, parentPath) {
+  if (!module.name) {
+    // eslint-disable-next-line no-console
+    console.error('WARNING: %s required from %s is missing', name, parentPath.join('->'))
+    return
+  }
+
+  const explicitName = `${module.name}@${module.version}`
   // Abort recursion if cycle found
   if (parentPath.includes(explicitName)) return
-  if (parentPath.length && !module.private) yield main
+
+  if (parentPath.length && !module.private) {
+    const explicitLicense = _extractLicenseName(
+      module.license || module.licence || module.licenses || module.licences)
+    const guessedLicense = explicitLicense ? null : guessModuleLicense(module)
+    const isLicenseGuessed = !!guessedLicense
+    const licenseDescriptor = explicitLicense || guessedLicense
+
+    yield {
+      name: module.name,
+      version: module.version,
+      explicitName,
+      licenseDescriptor,
+      isLicenseGuessed,
+      installPaths: [module.path],
+      dependencyPaths: [parentPath],
+    }
+  }
   if (!module.dependencies) return
 
   const subpath = [...parentPath, explicitName]
-  for (const dep of values(module.dependencies)) {
-    yield* iterateModules(dep, subpath)
+  for (const depName of Object.keys(module.dependencies)) {
+    yield* iterateModules(depName, module.dependencies[depName], subpath)
   }
 }
 
 export default (module) => {
   const moduleMap = {}
-  for (const _module of iterateModules(module, [])) {
-    const { explicitName } = _module
-    if (moduleMap[explicitName]) {
-      moduleMap[explicitName] = mergeWith(moduleMap[explicitName], _module, _mergeArray)
+  const circulars = {}
+  for (const _module of iterateModules('ROOT', module, [])) {
+    const { explicitName, licenseDescriptor } = _module
+    if (licenseDescriptor === '[Circular]') {
+      circulars[explicitName] = true
     } else {
-      moduleMap[explicitName] = _module
+      moduleMap[explicitName] = merge(_module, moduleMap[explicitName])
+    }
+  }
+  for (const circular of Object.keys(circulars)) {
+    if (!moduleMap[circular]) {
+      throw new Error(`${circular} has only circular references, cannot determine license`)
     }
   }
   return moduleMap
