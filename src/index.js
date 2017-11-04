c9b341640d79e87218a35ca966ac2b9bfbfa4fb2
Marcus Mennemeier <marcus.mennemeier@actano.de>
2017-11-05 00:27:56 +0100

    Add new exports for synchronous inspection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import detect from './lib/detect'
 import getViolating from './lib/violating'
 
+export { default as isWhitelisted } from './lib/whitelisted'
+export { default as getViolating } from './lib/violating'
+
 export const getLicenses = async (dev = false, path) => detect(dev, path)
 
 export default async function (whitelist = {}, { dev = false, path } = {}) {
